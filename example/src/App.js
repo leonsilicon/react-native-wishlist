6b93071e418d151a4963dc94c824e0e2b47871e8
Leon Si <leon@leonsilicon.com>
2026-05-12 08:58:16 -0400

    example expo app

--- example/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Chat from './Chat/ChatExample';
-// import { AssetListExample } from './AssetList/AssetListExample';
-export function App() {
-    return (React.createElement(GestureHandlerRootView, { style: { flex: 1 } },
-        React.createElement(Chat, null)));
-}
