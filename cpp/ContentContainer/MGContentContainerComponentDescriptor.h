c3e3c540ffb50130d5c51a0281f31c23e6bbe957
Leon Si <leon@leonsilicon.com>
2026-05-12 15:17:06 -0400

    ai fix?

--- cpp/ContentContainer/MGContentContainerComponentDescriptor.h
+++ cpp/ContentContainer/MGContentContainerComponentDescriptor.h
@@ -8,28 +8,24 @@
 namespace facebook {
 namespace react {
 
-class MGContentContainerComponentDescriptor
-    : public ConcreteComponentDescriptor<MGContentContainerShadowNode> {
+class MGContentContainerComponentDescriptor : public ConcreteComponentDescriptor<MGContentContainerShadowNode> {
   using ConcreteComponentDescriptor::ConcreteComponentDescriptor;
 
-  std::shared_ptr<ShadowNode> cloneShadowNode(
-      const ShadowNode &sourceShadowNode,
-      const ShadowNodeFragment &fragment) const override {
+  std::shared_ptr<ShadowNode> cloneShadowNode(const ShadowNode &sourceShadowNode, const ShadowNodeFragment &fragment)
+      const override
+  {
     // React holds on to old shadow nodes so we need to make sure to get the
     // latest state when cloning those to get the children that were set by
     // Wishlist.
-    auto &mostRecentStateData = static_cast<ConcreteState const *>(
-                                    sourceShadowNode.getMostRecentState().get())
-                                    ->getData();
+    auto &mostRecentStateData =
+        static_cast<ConcreteState const *>(sourceShadowNode.getMostRecentState().get())->getData();
     auto wishlistChildren = mostRecentStateData.wishlistChildren;
-    auto children =
-        wishlistChildren
-            ? std::make_shared<std::vector<std::shared_ptr<const ShadowNode>>>(
-                  *wishlistChildren)
-            : fragment.children;
+    auto children = fragment.children
+        ? fragment.children
+        : (wishlistChildren ? std::make_shared<std::vector<std::shared_ptr<const ShadowNode>>>(*wishlistChildren)
+                            : fragment.children);
     auto shadowNode = std::make_shared<MGContentContainerShadowNode>(
-        sourceShadowNode,
-        ShadowNodeFragment{fragment.props, children, fragment.state});
+        sourceShadowNode, ShadowNodeFragment{fragment.props, children, fragment.state});
 
     adopt(*shadowNode);
     return shadowNode;
