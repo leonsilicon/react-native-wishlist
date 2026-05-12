694891ffabb86b44734612a24cdfc55994ff93bb
Leon Si <leon@leonsilicon.com>
2026-05-12 06:36:08 -0400

    fix android build

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,9 +1,7 @@
-const path = require('path');
-
-module.exports = {
-  dependencies: {
-    wishlist: {
-      root: path.join(__dirname, '..'),
-    },
-  },
-};
+/**
+ * Keep this file minimal: `react-native-wishlist` is already declared in package.json
+ * as `"link:../"`. A second entry here under the name `wishlist` pointed at the same
+ * native code and caused CMake to run `add_subdirectory(... wishlist_autolinked_build)`
+ * twice with different source paths (duplicate binary dir error).
+ */
+module.exports = {};
